9c54412ed7af18e8d1c6e1c4ec81cbfdc3f933b8
ichili <uz.mikhail@gmail.com>
2017-04-17 01:10:04 +0300

    Initial commit for configuration manager

---
+++ config_manager.py
@@ -0,0 +1,41 @@
+import configparser
+import logging
+
+
+def _read_defaults():
+    import sys
+    import os.path
+    path = os.path.dirname(sys.argv[0])
+    path = os.path.join(path, 'Images')
+    settings = dict()
+    settings['ImagesDirectory'] = path
+    settings['count'] = 10
+    return settings
+
+
+def write_default_config():
+    config = configparser.ConfigParser()
+    config.read_dict(_read_defaults())
+    try:
+        config.write('settings.cfg')
+    except OSError:
+        logger.exception('Unable to write configuration file')
+
+
+def read_config():
+    settings = _read_defaults()
+    try:
+        with open('settings.cfg', 'r') as f:
+            config_string = '[all]\n' + f.read()
+    except OSError:
+        logger.exception('Cannot access configuration file. Using default values.')
+    else:
+        config = configparser.ConfigParser()
+        config.read_string(config_string)
+        options = config['all']
+        settings['path'] = options['ImagesDirectory']
+        settings['count'] = int(options['count'])
+    return settings
+
+
+logger = logging.getLogger('glogger')
