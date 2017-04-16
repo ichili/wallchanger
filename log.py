97712ca860d8aaac4195508fdcb12242f2813a0b
ichili <uz.mikhail@gmail.com>
2017-04-17 01:11:12 +0300

    Function for configuring global logger moved from main module

--- log.py
+++ log.py
@@ -1,12 +1,20 @@
 import logging
+import sys
+import os.path
 
 
 def setup_custom_logger(name, filename):
     formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(message)s')
-
     handler = logging.FileHandler(filename)
     handler.setFormatter(formatter)
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
     logger.addHandler(handler)
+    return logger
+
+
+def setup_global_logger():
+    logpath = os.path.dirname(sys.argv[0])
+    logpath = os.path.join(logpath, 'log.txt')
+    logger = setup_custom_logger('glogger', logpath)
     return logger
