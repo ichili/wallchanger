536f6c217110865699fce5b9ec5d4d25db6e9428
ichili <uz.mikhail@gmail.com>
2017-04-17 01:12:14 +0300

    Added basic exception handling

--- wallset.py
+++ wallset.py
@@ -1,18 +1,25 @@
 import ctypes
 import random
 import os.path
+import logging
 from os import walk
 
 
 def _set_wallpaper(path):
-    SPI_SETDESKWALLPAPER = 20
-    ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, path, 3)
+    try:
+        SPI_SETDESKWALLPAPER = 20
+        ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, path, 3)
+    except:
+        logger.exception('Error appeared while setting desktop wallpaper')
 
 
 def choose_wallpaper(path):
     files = []
-    for (dp, dn, fn) in walk(path):
-        files.extend(fn)
+    try:
+        for (dp, dn, fn) in walk(path):
+            files.extend(fn)
+    except OSError:
+        logger.exception('Error on acessing images directory')
     return random.choice(files)
 
 
@@ -20,3 +27,6 @@ def set_wallpaper(path):
     file = choose_wallpaper(path)
     file = os.path.join(path, file)
     _set_wallpaper(file)
+
+
+logger = logging.getLogger('glogger')
